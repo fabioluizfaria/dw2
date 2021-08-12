36012807c11914c1e9f77942d9cd4288d8277a25
Fabio Luiz Faria da Silva <65410834+fabioluizfaria@users.noreply.github.com>
2021-08-12 13:50:56 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const servidor = http.createServer(function (req, resp){
+     resp.end(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Desenvolvimento web II</h1>
+            </body>
+        </html>
+    `)
+});
+
+servidor.listen(3000);
