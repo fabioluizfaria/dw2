9bcc2c72cc7790b7c6fe30f4ebebcb0be2f813b2
Fabio Luiz Faria da Silva <65410834+fabioluizfaria@users.noreply.github.com>
2021-08-12 13:05:27 -0400

    05 Construindo o servidor web
    05 Construindo o servidor web

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-const express = require("express");
-const app = express();
+const http = require("http");
 
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-
-app.listen(3000,function(){
-    console.log('Servidor rodando na porta 3000');
-});
-
-app.get('/personalidadesNegras',function(req, resp){
-    resp.sendFile(__dirname + '/src/app/PersonalidadesNegras.html');
-});
-
-app.get('/',function(req, resp){
-    resp.send(
-        `
+const servidor = http.createServer(function (req, resp){
+     resp.end(`
         <html>
             <head>
                 <meta charset="utf-8">
@@ -22,57 +10,7 @@ app.get('/',function(req, resp){
                 <h1>Desenvolvimento web II</h1>
             </body>
         </html>
-        `
-    );
+    `)
 });
 
-app.get('/sobre',function(req, resp){
-    resp.send(
-        `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Sobre</h1>
-            </body>
-        </html>
-        `
-    );
-});
-
-// const servidor = http.createServer(function (req, resp){
-    
-//     let html = '';
-
-//     if (req.url == '/') {
-//         html = 
-//             `
-//                 <html>
-//                     <head>
-//                         <meta charset="utf-8">
-//                     </head>
-//                     <body>
-//                         <h1>Desenvolvimento web II</h1>
-//                     </body>
-//                 </html>
-//             `;
-//     } else if (req.url == '/sobre') {
-//         html = 
-//         `
-//             <html>
-//                 <head>
-//                     <meta charset="utf-8">
-//                 </head>
-//                 <body>
-//                     <h1>Sobre</h1>
-//                 </body>
-//             </html>
-//         `;
-//     }
-
-//     resp.end(html);
-
-// });
-
-// servidor.listen(3000);
+servidor.listen(3000);
