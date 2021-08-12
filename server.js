8005f9e6aa5049a481d5cbec85c855575c5ebebd
Fabio Luiz Faria da Silva <65410834+fabioluizfaria@users.noreply.github.com>
2021-08-12 13:52:57 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+app.listen(3000,function(){
+    console.log('Servidor rodando na porta 3000');
+});
+
+app.get('/',function(req, resp){
+    resp.send(
+        `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Desenvolvimento web II</h1>
+            </body>
+        </html>
+        `
+    );
+});
+
+app.get('/sobre',function(req, resp){
+    resp.send(
+        `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Sobre</h1>
+            </body>
+        </html>
+        `
+    );
+});
+
+// const servidor = http.createServer(function (req, resp){
+    
+//     let html = '';
+
+//     if (req.url == '/') {
+//         html = 
+//             `
+//                 <html>
+//                     <head>
+//                         <meta charset="utf-8">
+//                     </head>
+//                     <body>
+//                         <h1>Desenvolvimento web II</h1>
+//                     </body>
+//                 </html>
+//             `;
+//     } else if (req.url == '/sobre') {
+//         html = 
+//         `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1>Sobre</h1>
+//                 </body>
+//             </html>
+//         `;
+//     }
+
+//     resp.end(html);
+
+// });
+
+// servidor.listen(3000);
